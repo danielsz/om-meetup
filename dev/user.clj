6f9986f49eb78d4bd3a2d4353183c292deeaa894
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2014-09-21 21:51:54 +0300

    topics

--- dev/user.clj
+++ dev/user.clj
@@ -3,28 +3,29 @@
             [org.httpkit.client :as http]
             [environ.core :refer [env]]))
 
-
 (def base-url "http://api.meetup.com/2")
-
-(defn rsvped-users [] 
-  (let [rsvp-url (str base-url "/rsvps?event_id=200987152&order=name&key=" (:api-key env))]
-                        (-> @(http/get rsvp-url)
-                            (:body)
-                            (json/read-str :key-fn keyword)
-                            (:results))))
 
 (def results-xform
   (map (comp 
         #(json/read-str % :key-fn keyword) 
         :body)))
 
+(defn rsvped-users []
+  (let [rsvp-url (str base-url "/rsvps?event_id=200987152&order=name&key=" (:api-key env))]
+    (->> [@(http/get rsvp-url)]
+         (into {} results-xform)                     
+         (:results))))
+
 (defn users [] 
   (let [urls (map #(str base-url "/member/" (:member_id (:member %)) "?key=" (:api-key env)) (rsvped-users))
-                     futures (doall (map http/get urls))
-                     results (doall (map deref futures))]
-                 (sequence results-xform results)))
+        futures (doall (map http/get urls))
+        results (doall (map deref futures))]
+    (sequence results-xform results)))
 
 (defn app-state [] 
-  {:users (mapv #(select-keys % [:name :bio :photo :city :link]) (users))})
-
-
+  (let [users (users)
+        topics (map #(map :name %)(map :topics users))
+        users (map #(select-keys % [:name :id :bio :photo :city :link]) users)
+        coll (partition 2 (interleave users topics))]
+    {:users (for [x coll]
+              (assoc (first x) :topics (into [] (last x))))}))
